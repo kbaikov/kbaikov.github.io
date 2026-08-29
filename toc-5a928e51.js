8e63d17d9ce0e9325a9c504e2ad1b1eddfce0073
kbaikov <kbaikov@users.noreply.github.com>
2026-08-29 19:01:37 +0000

    deploy: 7bb8e7246f669e64a7a85129c6ce34e8d1b10be2

--- toc-5a928e51.js
+++ toc-5a928e51.js
@@ -8,7 +8,7 @@ class MDBookSidebarScrollbox extends HTMLElement {
         super();
     }
     connectedCallback() {
-        this.innerHTML = '<ol class="chapter"><li class="chapter-item expanded "><span class="chapter-link-wrapper"><a href="chapter_1.html"><strong aria-hidden="true">1.</strong> Chapter 1</a></span></li><li class="chapter-item expanded "><span class="chapter-link-wrapper"><a href="chapter_2.html"><strong aria-hidden="true">2.</strong> Chapter 2</a></span></li><li class="chapter-item expanded "><span class="chapter-link-wrapper"><a href="rss.html"><strong aria-hidden="true">3.</strong> RSS</a></span></li></ol>';
+        this.innerHTML = '<ol class="chapter"><li class="chapter-item expanded "><span class="chapter-link-wrapper"><a href="chapter_1.html"><strong aria-hidden="true">1.</strong> Chapter 1</a></span></li><li class="chapter-item expanded "><span class="chapter-link-wrapper"><a href="weight.html"><strong aria-hidden="true">2.</strong> Weight</a></span></li><li class="chapter-item expanded "><span class="chapter-link-wrapper"><a href="rss.html"><strong aria-hidden="true">3.</strong> RSS</a></span></li></ol>';
         // Set the current, active page, and reveal it if it's hidden
         let current_page = document.location.href.toString().split('#')[0].split('?')[0];
         if (current_page.endsWith('/')) {
