8ff149bae28ca97d942a8b0a16309a6447e1de50
kbaikov <kbaikov@users.noreply.github.com>
2026-09-21 05:10:35 +0000

    deploy: 5f6db2c45c37f5d4b902f338e16e1d05d41b539e

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-d94f4f23.js',
+                path_to_root + 'searchindex-087012ff.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
